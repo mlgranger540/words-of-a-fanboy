90f538e570465a9b740f3d5ef457a6867c608a17
mlgranger540 <mattgranger540@outlook.com>
2026-09-15 14:54:58 +0100

    Add navbar JS to Topics page and fix responsiveness of fandom boxes

--- public/assets/js/topics.js
+++ public/assets/js/topics.js
@@ -31,9 +31,44 @@ window.onload = async function(){
             let fandomImageAlt = fandom.fandomImageAlt;
 
             // Add content to fandom box
-            fandomBox += `<div class="fandom-box col-3" style="background-image: url(${fandomImageSrc}); background-size: cover">`;
+            fandomBox += `<div class="fandom-box col-xl-3 col-lg-4 col-md-6 col-12" style="background-image: url(${fandomImageSrc}); background-size: cover">`;
             fandomBox += `<h4 class="fandom-name">${fandomName}</h4></div>`
         })
         fandomRow.innerHTML = fandomBox;
     })
+
+    // Sticky navbar
+    var navbar = document.getElementById("navbar");
+    var offsetTop = navbar.offsetTop;
+    
+    // When scrolling past navbar, add sticky class and remove transparency
+    // When scrolling back up, remove sticky class and add transparency
+    function stick(){
+        if (window.scrollY >= offsetTop) {
+            navbar.classList.remove("absolute");
+            navbar.classList.add("sticky");
+            navbar.style.backgroundColor = "rgba(49, 51, 74, 1)";
+        } else {
+            navbar.classList.remove("sticky");
+            navbar.classList.add("absolute");
+            navbar.style.backgroundColor = "rgba(49, 51, 74, 0.5)";
+        }
+    }
+
+    window.addEventListener("scroll", stick);
+
+    // Add current year to copyright line
+    var year = new Date().getFullYear();
+    document.getElementById("year").innerHTML = year + " ";
+}
+
+// If collapsed navbar content is visible, make it not visible on click
+// If it's not visible, make it visible
+function openNav(){
+    var collapsedNavbar = document.getElementById("collapsed-content");
+    if (collapsedNavbar.style.display === "block") {
+        collapsedNavbar.style.display = "none";
+    } else {
+        collapsedNavbar.style.display = "block";
+    }
 }
